417d2982a9505a92ffc1006abb470be100a208f7
aqiel14 <72314836+aqiel14@users.noreply.github.com>
2021-07-24 14:35:27 +0700

    fix conflict

--- models/product_schema.js
+++ models/product_schema.js
@@ -6,12 +6,8 @@ const schema = mongoose.Schema({
   price: Number,
   profit: { type: Number, default: 0 },
   image: String,
-<<<<<<< HEAD
   user_id: [{ type: mongoose.Schema.Types.ObjectId, ref: 'users' }],
   bahan: [{ type: mongoose.Schema.Types.ObjectId, ref: 'bahan' }],
-=======
-  user_id: { type: mongoose.Schema.Types.ObjectId, ref: 'users' },
->>>>>>> 9d8c3ca (update kasir)
   created: { type: Date, default: Date.now },
 });
 
