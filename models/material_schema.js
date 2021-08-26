ca18afa4cf2ecff75af333b6eba9447ff340c004
ilham-primayadi <74481552+ilham-primayadi@users.noreply.github.com>
2021-08-27 02:53:30 +0700

    jwt

--- models/material_schema.js
+++ models/material_schema.js
@@ -6,9 +6,7 @@ const schema = mongoose.Schema({
   qty: Number,
   unit: String,
   total: Number,
-  bahan: [
-    { type: mongoose.Schema.Types.ObjectId, ref: "bahan", required: true },
-  ],
+  bahan: [{ type: mongoose.Schema.Types.ObjectId, ref: "bahan" }],
   user_id: [{ type: mongoose.Schema.Types.ObjectId, ref: "users" }],
   created: { type: Date, default: Date.now },
 });
